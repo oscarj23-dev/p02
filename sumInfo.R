4575789ac0bbc9d9d47ce5ae74ebe62b4b44c64e
Kaylina Saetern <kaylinasaetern@MacBook-Pro-413.local>
2023-02-20 17:55:40 -0800

    completed sum info and chart3

---
+++ sumInfo.R
@@ -0,0 +1,3 @@
+# SUMMARY INFORMATION
+# --------------------------------------------
+# These datasets include the obesity rates found in the US as well as fast food restaurants. From the information found in these datasets, we are able to compared how the number of fast food resturants in each state impact the level of obesity rates. 
